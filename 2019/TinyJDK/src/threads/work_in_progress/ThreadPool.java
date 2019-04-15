d9055dec0cea6c7822e204e14dae97609d6836b2
Alvise Spanò <spano.alvise@gmail.com>
2019-04-15 17:20:13 +0200

    ThreadPool in classe

--- 2019/TinyJDK/src/threads/work_in_progress/ThreadPool.java
+++ 2019/TinyJDK/src/threads/work_in_progress/ThreadPool.java
@@ -1,5 +1,9 @@
 package threads.work_in_progress;
 
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Objects;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
@@ -7,14 +11,44 @@ public class ThreadPool {
     private Queue<PooledThread> q = new ConcurrentLinkedQueue<>();
 
     public static class PooledThread extends Thread {
-        private Runnable r;
+        @Nullable
+        private Runnable currentRunnable;
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    synchronized (this) {
+                        print("waiting...");
+                        wait();
+                        print("awaken");
+                    }
+                    Objects.requireNonNull(currentRunnable).run();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+
+        @Override
+        public String toString() {
+            return String.format("[%s:%d]", this.getName(), this.getId());
+        }
+
     }
 
     public PooledThread acquire(Runnable cb) {
+        @NotNull final PooledThread p;
         if (q.isEmpty()) {
-            return new PooledThread();
+            System.out.println("spawning");
+            p = new PooledThread();
+            p.start();
         } else {
-            PooledThread p = q.poll();
+            p = q.poll();
+        }
+        synchronized (p) {
+            print("notify");
+            p.currentRunnable = cb;
             p.notify();
             return p;
         }
@@ -24,20 +58,20 @@ public class ThreadPool {
         q.add(t);
     }
 
-    public static void threadMain(PooledThread p) {
-        while (true) {
-            try {
-                p.wait();
-                p.r.run();  // TODO: potrebbe essere null
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     public static void main(String[] args) {
         ThreadPool pool = new ThreadPool();
         Thread t1 = pool.acquire(() -> {
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            print("bye");
         });
+    }
+
+    private static void print(String s) {
+        System.out.println(String.format("%s: %s", Thread.currentThread(), s));
     }
 }
