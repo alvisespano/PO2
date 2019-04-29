617920c54c4b23d3600261789d4f9be7eb07ba5d
Alvise Spanò <spano.alvise@gmail.com>
2019-04-29 17:18:06 +0200

    Consumer-producer

---
+++ 2019/TinyJDK/src/patterns/consumerproducer/Main.java
@@ -0,0 +1,61 @@
+package patterns.consumerproducer;
+
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class Main {
+
+    private static BlockingQueue<String> q = new LinkedBlockingQueue<>();
+    private static Random rand = new Random();
+
+    public static class Consumer extends Thread {
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(rand.nextInt(500));
+
+                    String s = q.take();
+                    System.out.println("consumer: " + s);
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        }
+    }
+
+    public static class Producer extends Thread {
+
+        @Override
+        public void run() {
+            while (true) {
+                try {
+                    Thread.sleep(rand.nextInt(500));
+
+                    int len = rand.nextInt(10);
+                    String s = "";
+                    for (int i = 0; i < len; ++i) {
+                        s = s + String.format("%d", i);
+                    }
+                    System.out.println("producer: " + s);
+                    q.add(s);
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+
+        }
+    }
+
+    public static void main(String[] args) {
+        new Producer().start();
+        new Consumer().start();
+
+
+    }
+
+}
