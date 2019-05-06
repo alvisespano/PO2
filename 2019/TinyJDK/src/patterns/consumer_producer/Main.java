2bb4dd47dae2bb3997ef5fe5f922e2eb4cdb401f
Alvise Spanò <spano.alvise@gmail.com>
2019-05-06 17:18:12 +0200

    Multi consumer-producer

--- 2019/TinyJDK/src/patterns/consumer_producer/Main.java
+++ 2019/TinyJDK/src/patterns/consumer_producer/Main.java
@@ -1,13 +1,13 @@
 package patterns.consumer_producer;
 
-import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
 
 public class Main {
 
-    private static BlockingQueue<String> q = new LinkedBlockingQueue<>();
+    //private static BlockingQueue<String> q = new LinkedBlockingQueue<>();
+    private static List<String> q = new ArrayList<>();
     private static Random rand = new Random();
 
     public static void print(String s) {
@@ -21,13 +21,24 @@ public class Main {
             while (true) {
                 try {
                     Thread.sleep(rand.nextInt(500));
-
-                    String s = q.take();
-                    print(s);
-
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
+
+                String s;
+                int size;
+                synchronized (q) {
+                    if (q.isEmpty()) {
+                        try {
+                            q.wait();
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    s = q.remove(0);
+                    size = q.size();
+                }
+                print(String.format("%s (size = %d)", s, size));
             }
         }
     }
@@ -42,15 +53,18 @@ public class Main {
                     int len = rand.nextInt(10);
                     StringBuilder s = new StringBuilder();
                     for (int i = 0; i < len; ++i) {
-                        s.append(String.format("%d", i));
-/*                        int n = rand.nextInt(26);
-                        int m = Character.getNumericValue('a') + n;
+                        //s.append(String.format("%d", i));
+                        int n = rand.nextInt(26);
+                        int m = Character.getNumericValue('a') + n + 87;
                         char c = Character.toChars(m)[0];
-                        s = s + c;
-                         */
+                        s.append(c);
+
                     }
                     print(s.toString());
-                    q.add(s.toString());
+                    synchronized (q) {
+                        q.add(s.toString());
+                        q.notify();
+                    }
 
                 } catch (Exception e) {
                     e.printStackTrace();
@@ -60,9 +74,18 @@ public class Main {
     }
 
     public static void main(String[] args) {
-            new Producer().start();
-            new Consumer().start();
-            print("byebye");
+        for (int i = 0; i < 10; ++i) {
+            Producer p = new Producer();
+            p.setName(String.format("producer-%d", i));
+            p.start();
+
+            Consumer c = new Consumer();
+            c.setName(String.format("consumer-%d", i));
+            c.start();
+        }
+
+
+        print("byebye");
     }
 
 }
