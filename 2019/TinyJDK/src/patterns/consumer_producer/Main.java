c2737f4b2e950d129289b378fe224c5c51223e9a
Alvise Spanò <spano.alvise@gmail.com>
2019-05-09 16:24:13 +0200

    Update Main.java

--- 2019/TinyJDK/src/patterns/consumer_producer/Main.java
+++ 2019/TinyJDK/src/patterns/consumer_producer/Main.java
@@ -1,10 +1,64 @@
 package patterns.consumer_producer;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
+import java.util.function.BiFunction;
+import java.util.function.Function;
 
 public class Main {
+
+    int f(int x) {
+        return this.f("ciao");
+    }
+
+    int f(String x) {
+        return this.f(1);
+    }
+
+    static <T> void quicksort3(List<? extends Comparable<T>> l) {
+
+    static <T extends Comparable<T>> void quicksort3(List<T> l) {
+        T a = l.get(0), b = l.get(1);
+        if (a.compareTo(b) < 0)
+
+    }
+
+    static void quicksort3(List<? extends Comparable<?>> l) {
+        Object a = l.get(0), b = l.get(1);
+        if (a. < 0)
+
+    }
+
+    static void quicksort(List<Comparable<Comparable<C>>> l) {
+    }
+
+    static <T> void quicksort(List<T> l, Comparator<T> f) {
+        T a = l.get(0), b = l.get(1);
+        if (f.compare(a, b) < 0)
+        // algo
+    }
+
+    static <T> void quicksort2(List<T> l, BiFunction<T, T, Integer> f) {
+        quicksort(l, new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                return f.apply(o1, o2);
+            }
+        });
+
+    }
+
+    static <T> void quicksort(List<T> l, BiFunction<T, T, Boolean> f) {
+        quicksort(l, new Comparator<T>() {
+            @Override
+            public int compare(T o1, T o2) {
+                return f.apply(o1, o2) ? -1 : 1;
+            }
+        });
+    }
+
 
     //private static BlockingQueue<String> q = new LinkedBlockingQueue<>();
     private static List<String> q = new ArrayList<>();
