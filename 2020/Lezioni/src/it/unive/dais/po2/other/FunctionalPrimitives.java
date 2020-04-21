703d68fdfc57a33bd450be87951618f46ee8c827
Alvise Spanò <spano.alvise@gmail.com>
2020-04-21 15:08:16 +0200

    Lezione 17

--- 2020/Lezioni/src/it/unive/dais/po2/other/FunctionalPrimitives.java
+++ 2020/Lezioni/src/it/unive/dais/po2/other/FunctionalPrimitives.java
@@ -6,13 +6,13 @@ import it.unive.dais.po2.zoo.Dog;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.List;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 
 public class FunctionalPrimitives {
 
-    //public static <A, B, C extends B> List<B> map(Iterable<A> l, Function<? super A, C          > f) {
     public static <A, B> List<B> map(Iterable<A> l, Function<? super A, ? extends B> f) {
         List<B> c = new ArrayList<>();
         for (A a : l) {
@@ -20,6 +20,13 @@ public class FunctionalPrimitives {
             c.add(b);
         }
         return c;
+    }
+
+    public static <A, B> List<B> map2(Iterable<A> l, Function<? super A, ? extends B> f) {
+        return fold(l, new ArrayList<>(), (x, acc) -> {
+            acc.add(f.apply(x));
+            return acc;
+        });
     }
 
     public static <A, B> B fold(Iterable<A> l, B zero, BiFunction<A, B, B> f) {
@@ -30,7 +37,43 @@ public class FunctionalPrimitives {
         return acc;
     }
 
+    public static <A, B> B fold_recur(Iterable<A> l, B zero, BiFunction<A, B, B> f) {
+        return fold_recur(l.iterator(), zero, f);
+    }
+
+    public static <A, B> B fold_recur(Iterator<A> it, B zero, BiFunction<A, B, B> f) {
+        return it.hasNext() ? fold_recur(it, f.apply(it.next(), zero), f) : zero;
+    }
+
+    public static void f() {                    // function_f:
+        int i = 0;                              // li r0, #0
+        while (i < 10) {                        // loop:
+                                                // cmp r0, #10
+                                                // jeq exit
+            System.out.println("ciao " + i);    // call ...
+            i = i + 1;                          // add r0, #1, r0
+        }                                       // jmp loop
+                                                // exit:
+                                                //      balbalbla
+    }
+
+    public static void g(int i) {               // function_g:
+        if (i > 0) {                            // ld sp, r0
+                                                // cmp r0, #0
+                                                // jeq exit
+            System.out.println("ciao " + i);    // call ...
+            g(i - 1);                         // sub r0, #1
+                                                // st r0, sp
+        }                                       // jmp function_g
+                                                // exit:
+                                                //    blablabla
+    }
+
     public static void main(String[] args) {
+
+        g(10);                                  // st #10, sp
+                                                  // jmp function_g
+
         List<Integer> l1 = new ArrayList<>();
         for (int i = 0; i < 10; ++i) {
             l1.add(i);
