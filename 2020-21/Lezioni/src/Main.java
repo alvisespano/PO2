a4679110bf79030437e92d3a9f127d6a090f5dfe
Alvise Spanò <spano.alvise@gmail.com>
2021-02-08 17:26:19 +0100

    Lezione 1

--- 2020-21/Lezioni/src/Main.java
+++ 2020-21/Lezioni/src/Main.java
@@ -1,5 +1,37 @@
 
 public class Main {
 
+    public static class Animal {
+        protected int weight;
+
+        public void eat(Animal a) {
+            this.weight += a.weight;
+        }
+    }
+
+    public static class Dog extends Animal {
+        public void bark() { }
+
+        public void marry(Dog d) {}
+
+        @Override
+        public void eat(Animal a) {
+            this.weight += a.weight / 2;
+        }
+    }
+
+    private static Animal makeAnimal() {
+        return new Dog();
+    }
+
+    public static void main(String[] args) {
+
+        Animal fido = makeAnimal();
+        Dog lilly = new Dog();
+        fido.eat(lilly);        // SUBSUMPTION
+
+        lilly.marry(fido);      // non compila perché fido : Animal
+
+    }
 
 }
