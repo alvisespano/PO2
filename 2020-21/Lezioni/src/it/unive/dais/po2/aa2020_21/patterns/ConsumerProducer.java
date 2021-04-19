28929fb2616c4d70c14ccd9cba6280688c4262ec
Alvise Spanò <spano.alvise@gmail.com>
2021-04-19 17:27:32 +0200

    Lezione 19/4/21

--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/patterns/ConsumerProducer.java
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/patterns/ConsumerProducer.java
@@ -1,34 +1,57 @@
 package it.unive.dais.po2.aa2020_21.patterns;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
 public class ConsumerProducer {
 
-    public static void loop() {
-        for (int i = 0; i < 10; ++i) {
-            System.out.printf("thread#%d: %d\n", Thread.currentThread().getId(), i);
-            try {
-                Thread.sleep(300);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+    private static final BlockingQueue<Integer> l = new LinkedBlockingQueue<>();
+
+    public static class Consumer extends Thread {
+
+        @Override
+        public void run() {
+            System.out.println("consumer started");
+            while (true) {
+                try {
+                    int n = l.take();
+                    System.out.printf("consumer: %d\n", n);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
 
-    public static class MyThread extends Thread {
-
+    public static class Producer extends Thread {
         @Override
         public void run() {
-            loop();
+            System.out.println("producer started");
+            Random rnd = new Random();
+            while (true) {
+                l.add(rnd.nextInt() % 100);
+            }
         }
-
     }
 
     public static void main(String[] args) {
-        Thread t1 = new MyThread();
-        Thread t2 = new MyThread();
-        t1.start();
-        t2.start();
+        try {
 
-        loop();
+            Thread c1 = new Consumer();
+            Thread p1 = new Producer();
+
+            c1.start();
+            p1.start();
+
+            c1.join();
+            p1.join();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 
---
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/patterns/ThreadTest.java
@@ -0,0 +1,46 @@
+package it.unive.dais.po2.aa2020_21.patterns;
+
+public class ThreadTest {
+
+    public static void loop() {
+        for (int i = 0; i < 10; ++i) {
+            System.out.printf("thread#%d: %d\n", Thread.currentThread().getId(), i);
+            try {
+                Thread.sleep(300);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public static class MyThread extends Thread {
+
+        @Override
+        public void run() {
+            loop();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        Thread t1 = new MyThread();
+        Thread t2 = new MyThread();
+        t1.start();
+        t2.start();
+
+        new Thread(() -> { loop(); }).start();
+
+        loop();
+
+
+        try {
+            t1.join();
+            t2.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+}
