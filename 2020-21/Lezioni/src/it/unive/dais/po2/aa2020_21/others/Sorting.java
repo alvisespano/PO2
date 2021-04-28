84fc32ea157419512a2ef958d584dc11b659b6da
Alvise Spanò <spano.alvise@gmail.com>
2021-04-28 15:31:35 +0200

    Lezione 28/4/2021

---
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/others/Sorting.java
@@ -0,0 +1,84 @@
+package it.unive.dais.po2.aa2020_21.others;
+
+import it.unive.dais.po2.aa2020_21.generics.Zoo;
+import java.util.*;
+
+public class Sorting {
+
+
+    public static void main(String[] args) {
+
+        Collection<Zoo.Animal> c = new ArrayList<>();
+
+        Zoo.Dog fido = new Zoo.Dog();
+        Zoo.Dog pluto = new Zoo.Dog();
+        c.add(fido);
+
+        // static <T> boolean addAll(Collection<? super T> c, T... elements)
+        //Collections.addAll(c, fido, pluto);   // TODO da rivedere dopo
+
+        List<Cane> c2 = new ArrayList<>();
+        sort(c2);
+
+        List<Girasole> c3 = new ArrayList<>();
+        sort(c3, (o1, o2) -> o1.foglie - o2.foglie);
+
+
+    }
+
+    public static class Pianta {
+        public int foglie;
+    }
+
+    public static class Girasole extends Pianta {}
+
+
+    public static class Animale implements Comparable<Animale> {
+        protected int peso;
+        public Animale(int peso) {
+            this.peso = peso;
+        }
+        public void eat(Animale a) {
+            this.peso += a.peso;
+        }
+
+        @Override
+        public int compareTo(Animale x) {
+            return this.peso - x.peso;
+        }
+    }
+
+    public static class Cane extends Animale {
+        protected int peli;
+
+        public Cane(int peso, int peli) {
+            super(peso);
+            this.peli = peli;
+        }
+
+        @Override
+        public int compareTo(Animale x) {
+            if (x instanceof Cane) {
+                Cane y = (Cane) x;
+                return (this.peso * this.peli) - (y.peso * y.peli);
+            }
+            return super.compareTo(x);
+        }
+
+    }
+
+
+    interface __Comparable<T> {
+        int compareTo(T x);
+    }
+
+    static <T extends Comparable<? super T>> void sort(List<T> list) {
+        Collections.sort(list);
+    }
+
+    static <T>                       void sort(List<T> list, Comparator<? super T> c) {
+        Collections.sort(list, c);
+    }
+
+
+}
--- 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/patterns/ThreadPool.java
+++ 2020-21/Lezioni/src/it/unive/dais/po2/aa2020_21/patterns/ThreadPool.java
@@ -3,11 +3,13 @@ package it.unive.dais.po2.aa2020_21.patterns;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 public class ThreadPool {
 
     private final List<PooledThread> threads = new ArrayList<>();
+    private long timeout = 5L * 60 * 1000;
 
     public ThreadPool() {}
 
@@ -15,9 +17,28 @@ public class ThreadPool {
         // TODO
     }
 
+    public void setTimeoutInMillis(long tm) {
+        timeout = tm;
+    }
+
+    // COME SI DEZUCCHERA IL COSTRUTTO SYNCHRONIZED
+    /*public synchronized void m1() {
+        synchronized (this) {
+            // blocco
+        }
+    }
+    public static synchronized void m2() {
+        Class<?> c = ThreadPool.class;
+        synchronized (c) {
+            // blocco
+        }
+    }*/
+
     private class PooledThread extends Thread {
         @NotNull
         private Runnable r;
+
+        private long lastUsed = System.currentTimeMillis();
 
         public PooledThread(Runnable r) {
             this.r = r;
@@ -31,7 +52,21 @@ public class ThreadPool {
                 } catch (Exception e) {
                     System.out.printf("%s: exception caught\n", this);
                 }
-                threads.add(this);
+                synchronized (threads) {
+                    // threads.lock()
+                    // try {
+                    threads.add(this);
+                    lastUsed = System.currentTimeMillis();
+                    // }
+                    // finally { threads.unlock() }
+                }
+
+                // RESOURCE ALLOCATOR
+                /*
+                    PREAMBOLO               ad esempio lock
+                    try { BLOCCO }          ad esempio la critical section
+                    finally { EPILOGO }     ad esempio unlock
+                */
                 try {
                     wait(); // TODO controllare se la notify() può accadere prima della wait()
                 } catch (InterruptedException e) {
@@ -49,20 +84,26 @@ public class ThreadPool {
         public void setRunnable(Runnable r) {
             this.r = r;
         }
+
+        public boolean hasExpired() {
+            long now = System.currentTimeMillis();
+            return now - lastUsed > timeout;
+        }
     }
 
     @NotNull
     public Thread acquire(@NotNull Runnable r) {
         PooledThread t;
-        if (threads.isEmpty()) {
-            t = new PooledThread(r);
-            t.start();
-        }
-        else {
-            // TODO critical section
-            t = threads.remove(0);
-            t.setRunnable(r);
-            t.notify();
+        synchronized (threads) {
+            threads.removeIf(PooledThread::hasExpired);
+            if (threads.isEmpty()) {
+                t = new PooledThread(r);
+                t.start();
+            } else {
+                t = threads.remove(0);
+                t.setRunnable(r);
+                t.notify();
+            }
         }
         return t;
     }
