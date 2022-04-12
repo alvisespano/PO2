43c6647b6d370be477131c4ef3dd10324582860b
Alvise Spanò <spano.alvise@gmail.com>
2022-04-12 17:16:20 +0200

    Update ConsumerProducer.java

--- 2021-22/Lezioni21-22/src/ConsumerProducer.java
+++ 2021-22/Lezioni21-22/src/ConsumerProducer.java
@@ -9,15 +9,17 @@ public class ConsumerProducer {
     public static List<Integer> q = new ArrayList<>();
 
     public static class Producer extends Thread {
+
         @Override
         public void run() {
             while (true) {
                 try {
                     Thread.sleep(rand.nextLong(100L));
-                    synchronized (q) {
+
+                    synchronized (q) {  // LOCK
                         q.add(rand.nextInt(100));
                         q.notify();
-                    }
+                    }  // UNLOCK
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -29,7 +31,7 @@ public class ConsumerProducer {
         @Override
         public void run() {
             while (true) {
-                synchronized (q) {
+                synchronized (q) { // LOCK
                     if (q.isEmpty()) {
                         try {
                             q.wait();
@@ -39,20 +41,23 @@ public class ConsumerProducer {
                     }
                     int n = q.remove(0);
                     System.out.printf("consumed: %d (size = %d)\n", n, q.size());
-                }
+                } // UNLOCK
             }
         }
     }
 
     public static void main(String[] args) {
-        Producer p = new Producer();
-        Consumer c = new Consumer();
-        p.start();
-        c.start();
+        Producer p1 = new Producer();
+        Producer p2 = new Producer();
+        Consumer c1 = new Consumer();
+        Consumer c2 = new Consumer();
+        p1.start();
+        p2.start();
+        c1.start();
+        c2.start();
 
         try {
-            p.join();
-            c.join();
+            p1.join();
         } catch (InterruptedException e) {}
     }
 
