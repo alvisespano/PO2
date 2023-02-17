5eee376a889938aa1502bac03857fcc797ac0b64
Alvise Spanò <spano.alvise@gmail.com>
2023-02-17 15:19:50 +0100

    Lezioni Java 2022-23 First Push

---
+++ 2022-23/LezioniJava22-23/src/Generics.java
@@ -0,0 +1,88 @@
+public class Generics {
+
+    // JDK 1.0 (1994)
+
+    public interface Iterator {
+        boolean hasNext();
+        Object next();
+    }
+
+    public interface Iterable {
+        Iterator iterator();
+    }
+
+    public interface Collection extends Iterable {
+        int size();
+        void add(Object x);
+        void remove(Object x);
+        boolean contains(Object x);
+    }
+
+    public interface List extends Collection {
+        Object get(int index);
+        void set(int index, Object x);
+    }
+
+    public static class ArrayList implements List {
+
+        private Object[] a;
+        private int sz;
+
+        public ArrayList(int capacity) {
+            a = new Object[capacity];
+            sz = 0;
+        }
+
+        public ArrayList() {
+            this(10);
+        }
+
+        @Override
+        public int size() {
+            return sz;
+        }
+
+        @Override
+        public void add(Object x) {
+            if (sz >= a.length) {
+                Object[] old = a;
+                a = new Object[old.length * 2];
+                for (int i = 0; i < old.length; ++i)
+                    a[i] = old[i];
+            }
+            a[sz++] = x;
+        }
+
+        @Override
+        public void remove(Object x) {
+
+        }
+
+        @Override
+        public boolean contains(Object x) {
+            return false;
+        }
+
+        @Override
+        public Iterator iterator() {
+            return null;
+        }
+
+
+        @Override
+        public Object get(int index) {
+            return a[index];
+        }
+
+        @Override
+        public void set(int index, Object x) {
+            a[index] = x;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ 2022-23/LezioniJava22-23/src/Zoo.java
@@ -0,0 +1,42 @@
+public class Zoo {
+
+
+    public static class Animal {
+        protected int weight;
+
+        public Animal(int w) {
+            this.weight = w;
+        }
+
+        public void eat(Animal a) {
+            this.weight += a.weight;
+        }
+    }
+
+    public static class Dog extends Animal {
+        private String hairColor;
+
+        public Dog(int w, String hairColor) {
+            super(w);
+            this.hairColor = hairColor;
+        }
+
+        @Override
+        public void eat(Animal a) {
+            this.weight = a.weight * 2;
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Animal fido = new Dog(50, "Red");
+        Dog pluto = new Dog(20, "Dotted");
+        Animal ciccio = pluto;
+        Dog bobby = pluto;
+        ciccio = bobby;
+
+        ciccio.eat(new Dog(10, "White"));
+
+    }
+
+}
