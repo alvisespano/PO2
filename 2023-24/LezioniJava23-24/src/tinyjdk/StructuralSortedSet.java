29b4deb9603b503936716d5c1d9f3f7813bed68d
Alvise Spanò <spano.alvise@gmail.com>
2024-03-18 22:31:40 +0100

    Update StructuralSortedSet.java

--- 2023-24/LezioniJava23-24/src/tinyjdk/StructuralSortedSet.java
+++ 2023-24/LezioniJava23-24/src/tinyjdk/StructuralSortedSet.java
@@ -3,6 +3,7 @@ package tinyjdk;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.NoSuchElementException;
 
 public class StructuralSortedSet<T extends Comparable<T>>
         extends StructuralSet<T>
@@ -14,29 +15,37 @@ public class StructuralSortedSet<T extends Comparable<T>>
         sort();
     }
 
-    // static <T extends Comparable<T>> void sort(List<T> l);
-    //static <T> void sort(T[] a, Comparator<? super T> c) {}
+/*    private void sort() {
+        T[] a = (T[]) this.a;
+        while (...) {
+            int i = 0;
+            if (a[i].compareTo(a[i + 1]) > 0) {
+                T tmp = a[i];
+                a[i] = a[i + 1];
+                a[i + 1] = tmp;
+            }
+        }
+    }*/
+
     private void sort() {
         T[] src = (T[]) a;
-        T[] tmp = Arrays.copyOf(src, size());
-        Arrays.sort(tmp, new Comparator<T>() {
+        Arrays.sort(src, 0, size(), new Comparator<T>() {
             @Override
             public int compare(T o1, T o2) {
                 return o1.compareTo(o2);
             }
         });
-
-        // TODO ricopiare tmp in a
-
     }
 
     @Override
     public T first() {
-        return null;
+        if (isEmpty()) throw new NoSuchElementException();
+        return (T) a[0];
     }
 
     @Override
     public T last() {
-        return null;
+        if (isEmpty()) throw new NoSuchElementException();
+        return (T) a[size() - 1];
     }
 }
