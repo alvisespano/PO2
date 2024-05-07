dccfeea5bec0e9cfdcf4e9e19f75c8a3b492e8ce
Alvise Spanò <spano.alvise@gmail.com>
2024-05-07 10:22:05 +0200

    C++ matrix

--- 2023-24/LezioniCPP23-24/LezioniCPP/LezioniCPP/matrix.cpp
+++ 2023-24/LezioniCPP23-24/LezioniCPP/LezioniCPP/matrix.cpp
@@ -6,30 +6,59 @@ template <class T>
 class matrix
 {
 private:
+	size_t cols;
 	vector<T> v;
 
 public:
 	matrix() : v() {}
 	matrix(const matrix<T>& m) : v(m.v) {}
-	matrix(size_t rows, size_t cols) : v(rows * cols) {}
+	
+	
+//	matrix(size_t rows, size_t _cols) : cols(_cols), v(rows* cols) {}
+	matrix(size_t rows, size_t _cols, const T& x = T())
+		: cols(_cols), v(rows* cols, x) {}
+
+	
 	explicit matrix(size_t dim) : matrix(dim, dim) {}
 
-	const T& at(size_t i, size_t j) const
+	matrix<T>& operator=(const matrix<T>& m)
+	{
+		cols = m.cols;
+		v = m.v;
+		return *this;
+	}
+
+
+	const T& operator()(size_t i, size_t j) const
 	{
 		return v[i * cols + j];
 	}
-};
+	T& operator()(size_t i, size_t j)
+	{
+		return v[i * cols + j];
+	}
+
+	size_t get_cols() const { return cols; }
+	size_t get_rows() const { return v.size() / cols; }
+
+}; 
 
 
-void main() {
+void main() 
+{
+	matrix<int> m(20, 30);
+	m(8, 10) = m(3, 4);
 
-	int x;
-	x = 7;
-	int y = 8;
-	x = y;
+	matrix<int> m2(40, 50);
+	m = m2;
+	m.operator=(m2);
 
-	matrix<double> a;
-	a = matrix<double>();
-	matrix<double> b(...);
-	b = a;
+	m = m2 = m; //	m.operator=(m2.operator=(m));
+
+	matrix<int> m3;
+	m3 = m;
+	matrix<int> m4(m3);
+
 }
+
+
